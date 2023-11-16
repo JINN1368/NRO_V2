32fde50433a68cd93261c3b94c3771c0f58e7821
Jinn <52101245+JINN1368@users.noreply.github.com>
2023-11-16 12:42:02 +0700

    Delete Pro directory

--- Pro/src/info.py
+++
@@ -1,9 +0,0 @@
-input(f"""Phiên Bản Được Dựng Bởi JINN©2023
-• Facebook: fb.com/Ki3tngu
-• Zalo: 0853035354
-• Youtube: JINN1368
-• GITHUB: JINN1368
-• Phiên bản: 1.2.0m_2.4.2d
-> Cảm ơn bạn đã ủng hộ
-""")
-exit()
